e421ad8ef46e51a2e9a70e9d35bbb9d7b9c67c68
mirai <mirai@DESKTOP-6U1NKQP>
2024-11-22 18:38:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Maxim_Cazacu_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
